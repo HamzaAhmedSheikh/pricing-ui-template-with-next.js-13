f1f7503cdf97a24ef86c925dfcd3e35cd2354b85
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-01-19 03:34:21 +0500

    I have created a header for the pricing-ui template.

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export function Header() {
+  return(
+    <Box as="section" color="#F7FAFC"  bg="purple.600" pt='90px' pb='198px' px='32px'>
+       <Heading fontWeight="800" fontSize="48px">
+        Simple pricing for your business
+      </Heading>
+      <Text fontWeight="500" fontSize="24px" pt='16px'>
+        Plans that are carefully crafted to suit your business.
+      </Text>
+    </Box>
+  )   
+}
