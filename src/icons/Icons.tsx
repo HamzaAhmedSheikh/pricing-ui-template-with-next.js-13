ebb5daa492f004ca428e775f85f6d790159dc9e5
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-01-19 22:40:37 +0500

    Update Icons.tsx

--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -9,7 +9,7 @@ export function CheckIcon(props: any) {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clip-rule="evenodd"
         d="M11 22C17.0751 22 22 17.0751 22 11C22 4.92487 17.0751 0 11 0C4.92487 0 0 4.92487 0 11C0 17.0751 4.92487 22 11 22ZM6.2 10.0857L5 11.3714L9 16L17 8.28571L15.8 7L9 12.4L6.2 10.0857Z"
         fill="#652CD3"
@@ -37,7 +37,7 @@ export function MoneyBackGuaranteeIcon({ ...props }) {
         stroke="#CBD5E0"
         strokeWidth="4.8"
         stroke-miterlimit="10"
-        stroke-linecap="square"
+        strokeLinecap="square"
       />
     </svg>
   );
