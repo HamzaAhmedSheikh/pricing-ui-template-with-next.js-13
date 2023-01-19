790e0c2db369e5c8482623cc2912bbbeb7a0ec73
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-01-19 09:05:38 +0500

    I have designed a fully responsive pricing ui template and added a new page called feature to the existing one.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { ChakraProvider } from '@chakra-ui/react'
 
+
 export default function RootLayout({
   children,
 }: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 "use client"
 
-import { Box } from '@chakra-ui/react';
 import { Header  } from '../components/Header';
 import { Pricing } from '../components/Pricing';
+import { Features } from '../components/Features';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Pricing />
+      <Features />
     </div>
   )
 }
